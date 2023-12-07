0821706afbf73182b8a7c24993a4c50b62da9af9
Tapio Rautonen <trautonen@users.noreply.github.com>
2023-12-07 19:33:01 +0200

    add day 7

---
+++ src/day07/part1.ts
@@ -0,0 +1,92 @@
+import { loadInput, parseLines, sum } from '../utils/input'
+
+process.env['TEST'] = 'false'
+
+export type Labels = readonly string[]
+
+export type Label<C extends Labels> = C[number]
+
+export type Hand<C extends Labels> = [Label<C>, Label<C>, Label<C>, Label<C>, Label<C>]
+
+export type Round<C extends Labels> = {
+  hand: Hand<C>
+  bid: number
+}
+
+export type CountByLabel<C extends Labels> = (hand: Hand<C>) => number[]
+
+export type Comparator<T> = (a: T, b: T) => number
+
+export const toSortedCountsByLabel =
+  <C extends Labels>(labels: C) =>
+  (hand: Hand<C>): number[] => {
+    return labels.map(label => hand.filter(card => card === label).length).toSorted((a, b) => b - a)
+  }
+
+export const compareLabels =
+  <C extends Labels>(labels: C) =>
+  (left: Hand<C>, right: Hand<C>): number => {
+    for (let i = 0; i < left.length; i++) {
+      const leftLabelIndex = labels.indexOf(left[i]!)
+      const rightLabelIndex = labels.indexOf(right[i]!)
+      if (leftLabelIndex > rightLabelIndex) {
+        return -1
+      }
+      if (leftLabelIndex < rightLabelIndex) {
+        return 1
+      }
+    }
+    return 0
+  }
+
+export const compareHands =
+  <C extends Labels>(labels: C, counter: CountByLabel<C>) =>
+  (left: Hand<C>, right: Hand<C>): number => {
+    const leftSortedCounts = counter(left)
+    const rightCountsByLabel = counter(right)
+    if (leftSortedCounts[0]! > rightCountsByLabel[0]!) {
+      return 1
+    }
+    if (leftSortedCounts[0]! < rightCountsByLabel[0]!) {
+      return -1
+    }
+    if (leftSortedCounts[1]! > rightCountsByLabel[1]!) {
+      return 1
+    }
+    if (leftSortedCounts[1]! < rightCountsByLabel[1]!) {
+      return -1
+    }
+    return compareLabels(labels)(left, right)
+  }
+
+export const compareRounds =
+  <C extends Labels>(labels: C, comparator: Comparator<Hand<C>>) =>
+  (left: Round<C>, right: Round<C>): number => {
+    return comparator(left.hand, right.hand)
+  }
+
+export const isLabel =
+  <C extends Labels>(labels: C) =>
+  (value: string): value is Label<C> => {
+    return labels.some(label => label === value)
+  }
+
+export const parseRound =
+  <C extends Labels>(labels: C) =>
+  (data: string): Round<C> => {
+    const [cardsData, bidData] = data.split(/\s+/)
+    const [c1, c2, c3, c4, c5] = cardsData!.split('').filter(isLabel(labels))
+    return {
+      hand: [c1!, c2!, c3!, c4!, c5!],
+      bid: parseInt(bidData!, 10),
+    }
+  }
+
+export const solve = (): number => {
+  const labels = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'] as const
+  return parseLines(loadInput(import.meta.url))
+    .map(parseRound(labels))
+    .toSorted(compareRounds(labels, compareHands(labels, toSortedCountsByLabel(labels))))
+    .map((hand, rank) => hand.bid * (rank + 1))
+    .reduce(sum)
+}
---
+++ src/day07/part2.ts
@@ -0,0 +1,23 @@
+import { loadInput, parseLines, sum } from '../utils/input'
+import { Hand, Labels, compareHands, compareRounds, parseRound } from './part1'
+
+process.env['TEST'] = 'false'
+
+export const toSortedCountsByLabelWithJoker =
+  <C extends Labels>(labels: C) =>
+  (hand: Hand<C>): number[] => {
+    const counts = labels.map(label => hand.filter(card => card === label).length)
+    const jokerCounts = counts.at(-1)!
+    const labelCounts = counts.slice(0, -1).toSorted((a, b) => b - a)
+    labelCounts[0] = labelCounts[0]! + jokerCounts
+    return labelCounts
+  }
+
+export const solve = (): number => {
+  const labels = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J'] as const
+  return parseLines(loadInput(import.meta.url))
+    .map(parseRound(labels))
+    .toSorted(compareRounds(labels, compareHands(labels, toSortedCountsByLabelWithJoker(labels))))
+    .map((hand, rank) => hand.bid * (rank + 1))
+    .reduce(sum)
+}
